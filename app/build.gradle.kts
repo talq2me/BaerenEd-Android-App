818074d92fa1a6193faa55d76152a3d563d0f137
talq2me <tiffany.quinlan@gmail.com>
2025-11-18 20:49:48 -0500

    Release version 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.talq2me.baerened"
         minSdk = 23
         targetSdk = 35
-        versionCode = 12
-        versionName = "12"
+        versionCode = 13
+        versionName = "13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
