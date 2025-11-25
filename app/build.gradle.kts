9e0a5c9bf2d07406c3a19135e3a9fa36fb929ab5
talq2me <tiffany.quinlan@gmail.com>
2025-11-25 11:44:20 -0500

    Release version 53

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.talq2me.baerened"
         minSdk = 23
         targetSdk = 35
-        versionCode = 52
-        versionName = "52"
+        versionCode = 53
+        versionName = "53"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
