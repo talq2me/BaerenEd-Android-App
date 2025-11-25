ccb24a06a2d3b1947784f4625b5ae9fdc1858cd1
talq2me <tiffany.quinlan@gmail.com>
2025-11-25 12:10:38 -0500

    Update GitHub upload to use simple filenames (AM.txt, BM.txt) that overwrite instead of creating timestamped files. Added pull/rebase to release script to sync with app report uploads. Added commitMessage.txt for custom commit messages.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.talq2me.baerened"
         minSdk = 23
         targetSdk = 35
-        versionCode = 54
-        versionName = "54"
+        versionCode = 55
+        versionName = "55"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
