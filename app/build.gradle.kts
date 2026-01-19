cf2810aeb78a4145fa26c0b66187c865443cd4e8
talq2me <tiffany.quinlan@gmail.com>
2026-01-19 10:26:03 -0500

    refactor and attempt to fix all syncing issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.talq2me.baerened"
         minSdk = 23
         targetSdk = 35
-        versionCode = 105
-        versionName = "105"
+        versionCode = 106
+        versionName = "106"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
